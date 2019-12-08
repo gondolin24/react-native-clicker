7cb65faa3479c54c574f811f150809e3f3f76a5b
Eduardo Guzman-Diaz <tloz_tlotr@hotmail.com>
2019-12-07 19:58:15 -0500

    stuff

---
+++ clickRunner/app/screens/main/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import View from 'Reac'
+
+const Main = ()=>{
+
+    return(
+      <View>
+          <Text>
+              Hello
+          </Text>
+      </View>
+    );
+};
+export default Main;
